6b1cdd9746b966311167bd5d5c9fd1e4d99335be
mlietti1 <mcf.lietti@outlook.com>
2022-11-14 16:51:32 +0100

    primo commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppTitle
+  },
+  data(){
+    return{
+      title: 'Hello Vue + Vite'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <span>Stampo titolo da component qui sotto:</span>
+  </div>
+  <AppTitle/>
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'AppTitle',
+  data(){
+    return{
+      title: 'La mia prima App con Vite!'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<style>
+h1 {
+  text-align: center;
+  color: cornflowerblue;
+}
+</style>
